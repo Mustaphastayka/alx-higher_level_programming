630380a2fde2175ba4907bdc9d34d0407b153c5e
mustaphastayka <mustaphaelfarkh2000@gmail.com>
2023-11-28 04:40:08 -0800

    test

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(1 , 100):
+for i in range(1, 100):
     if i in range(1,10):
         print ("0{0}".format(i),end=", ")
     elif i == 99:
-        print ("{0}".format(i),)
+        print ("{0}".format(i))
     else:
         print ("{0}".format(i),end=", ")
